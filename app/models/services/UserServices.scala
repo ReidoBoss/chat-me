673d430d2a61d199fb7cfd72dd4cee776c680f54
ReidoBoss <bryansagarino222@gmail.com>
2024-11-08 16:55:05 +0800

    Feature\1: Added services init for adding a user

--- app/models/services/UserServices.scala
+++ app/models/services/UserServices.scala
@@ -1,3 +1,19 @@
 package services.user
 
+import repo.user.UserRepository
+import domain.user.{ User }
 
+import scala.concurrent.{ Future, ExecutionContext }
+import javax.inject._
+
+
+@Singleton
+class UserServices @Inject()(
+  userRepo: UserRepository
+)(using ExecutionContext){
+
+  def add (user:User) = {
+
+  }
+
+}
